a6948bd176b674aa186e3f5b3385886de3f9edde
Mateusz Bryll <mateusz.bryll@mateuszbryll.com>
2025-10-19 11:00:27 +0200

    chore: setup PrimeNG and Tailwind

--- src/frontend/src/app/app.config.ts
+++ src/frontend/src/app/app.config.ts
@@ -1,4 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,6 +10,12 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
   ]
 };
--- src/frontend/src/app/app.ts
+++ src/frontend/src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { Menubar } from 'primeng/menubar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [ Menubar, RouterOutlet ],
+  template: `
+  <div class="card">
+    <p-menubar [model]="menuItems">
+    </p-menubar>
+  </div>
+  <router-outlet></router-outlet>
+  `,
 })
-export class App {
-  protected readonly title = signal('TaskFlow.UI');
+export class App implements OnInit {
+  public menuItems: MenuItem[] | undefined;
+
+  public ngOnInit(): void {
+    this.menuItems = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+        routerLink: '/'
+      }
+    ];
+  }
 }
